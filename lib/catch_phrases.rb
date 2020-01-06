f9662e31de9645b19bfd5217b4f8f11ff4c51dbf
Timur Mishiev <timurmishiev@gmail.com>
2020-01-06 19:27:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,10 +2,14 @@ def catch_phrase(phrase)
   puts phrase
 end
 
-def toadstool
+def toadstool(status)
   puts status
 end
 
 def link(phrase)
   puts phrase
 end 
+
+def any_phrase(phrase)
+  puts phrase
+end
