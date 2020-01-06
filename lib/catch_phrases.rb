a4e06e349dede96b18269ade0e9bb8275cdf241d
Timur Mishiev <timurmishiev@gmail.com>
2020-01-06 19:19:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,6 +2,12 @@ def catch_phrase(phrase)
   puts phrase
 end
 
+
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
+
 def toadstool
   puts status
 end
